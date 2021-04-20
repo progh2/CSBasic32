2a86d571668e0814eb2e72217d456263437ab99b
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-20 11:52:46 +0900

    #1 기본 용어 실습

--- CSBasic32/Program.cs
+++ CSBasic32/Program.cs
@@ -8,8 +8,34 @@ namespace CSBasic32
 {
     class Program
     {
+        // var 키워드는 지역변수로만 사용 가능.
+        //var name = "함기훈";
+
         static void Main(string[] args)
         {
+            // keyword
+            //int as;
+
+            // context keyword
+            var name = "함기훈";
+            //var var = "함기훈";
+            //var abcs = "asdf";
+
+            // 식별자 Identifier 
+            int alpha;
+            int Alpha;
+            //int break;
+            //int 263alpha;
+            //int has space;
+            //int 🤣❤😒;
+            int 한글숫자;
+
+            // inline commnet 
+            /*
+             * comment
+             */
+
+            Console.WriteLine("Hello");
         }
     }
 }
