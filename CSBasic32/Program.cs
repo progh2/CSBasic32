dc17e75cb1f54c1ed06ba59390e1b93715514ac1
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-23 15:37:48 +0900

    #5 증감연산자 예시 - 전위/후위 주의

--- CSBasic32/Program.cs
+++ CSBasic32/Program.cs
@@ -151,6 +151,15 @@ namespace CSBasic32
 
             bool IsLive = true;
 
+            // 증감연산자 - 섞어서 쓸 경우
+            // 전위, 후위에 따라서 출력 값이 변할 수 있으니 주의
+            int num = 10;
+            Console.WriteLine(num++);
+            Console.WriteLine(++num);
+            Console.WriteLine(num--);
+            Console.WriteLine(--num);
+
+
         }
     }
 }
