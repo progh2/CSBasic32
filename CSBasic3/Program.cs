928dfbb1964ed38f1937438bf88203a70559669a
Gihun Ham <ham@e-mirim.hs.kr>
2021-05-13 09:41:50 +0900

    #20 foreach / 중첩 / break / continue 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -43,6 +43,34 @@ namespace CSBasic3
             {
                 Console.WriteLine(intArray3[k - 1]);
             }
+
+            string[] fruits = { "사과", "배", "포도", "딸기", "바나나","오렌지" };
+            foreach(var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            for(i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            for(i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10 - i - 1; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
