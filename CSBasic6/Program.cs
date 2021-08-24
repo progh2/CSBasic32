66713a7ffe94fb1c1aabb61c5e386f1954b24c9c
Gihun Ham <ham@e-mirim.hs.kr>
2021-08-24 12:09:44 +0900

    하이딩과 오버라이딩 비교 실습

--- CSBasic6/Program.cs
+++ CSBasic6/Program.cs
@@ -8,8 +8,65 @@ namespace CSBasic6
 {
     class Program
     {
+        class Animal
+        {
+            public void Eat(){   Console.WriteLine("냠냠");}
+            public virtual void Eat2() { Console.WriteLine("냠냠"); }
+        }
+        class Cat : Animal
+        {
+            public new void Eat() {  Console.WriteLine("냥냥"); }
+            public override void Eat2() { Console.WriteLine("냥냥"); }
+        }
+        class Dog : Animal
+        {
+            public new void Eat() { Console.WriteLine("멍멍"); }
+            public override void Eat2() { Console.WriteLine("멍멍"); }
+        }
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("난 부모 메서드");
+            }
+            public virtual void Method2()
+            {
+                Console.WriteLine("난 부모 메서드");
+            }
+        }
+
+        class Child : Parent
+        {
+            public new string variable = "hiding";
+            public new void Method()
+            {
+                Console.WriteLine("난 자식 메서드");
+            }
+            public override void Method2()
+            {
+                Console.WriteLine("난 자식 메서드");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child child = new Child();
+            //Console.WriteLine(child.variable);
+            //Console.WriteLine(((Parent)child).variable);
+            child.Method();
+            ((Parent)child).Method();
+
+            child.Method2();
+            ((Parent)child).Method2();
+
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Dog(), new Dog()
+            };
+            foreach(var item in Animals) { item.Eat(); }
+            foreach (var item in Animals) { item.Eat2(); }
         }
     }
 }
