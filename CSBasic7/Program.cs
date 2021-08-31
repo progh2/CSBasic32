9e7b37143918da7d44dd7cba7deda1bff5a18725
Gihun Ham <ham@e-mirim.hs.kr>
2021-08-31 10:51:56 +0900

    제네릭 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -8,10 +8,31 @@ namespace CSBasic7
 {
     class Program
     {
+        class Wanted<T>
+            //where T : Student
+        {
+            public T Value;
+            public Wanted(T value)
+            {
+                this.Value = value;
+            }
+        }
+        class Student
+        {
+            public override string ToString()
+            {
+                return "학생";
+            }
+        }
+
         static void Main(string[] args)
         {
-
-
+            Wanted<string> ws = new Wanted<string>("String");
+            Wanted<int> wi = new Wanted<int>(42);
+            Wanted<Student> wstu = new Wanted<Student>(new Student());
+            Console.WriteLine(ws.Value);
+            Console.WriteLine(wi.Value);
+            Console.WriteLine(wstu.Value);
         }
     }
 }
