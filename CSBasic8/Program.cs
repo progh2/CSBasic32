904e3e6bf73e8d6e3ea9ab6b70ad8b1d51d0404d
Gihun Ham <ham@e-mirim.hs.kr>
2021-09-02 09:35:14 +0900

    IComparable 인터페이스 구현 예제

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -8,8 +8,37 @@ namespace CSBasic8
 {
     class Program
     {
+        class Product : IComparable
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                return Price.CompareTo((obj as Product).Price);
+            }
+
+            public override string ToString()
+            {
+                return Name + " : " + Price + "원";
+            }
+        }
+
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>()
+            {
+                new Product(){ Name = "고구마", Price=1500},
+                new Product(){ Name = "사과", Price=2400},
+                new Product(){ Name = "바나나", Price=1000},
+                new Product(){ Name = "배", Price=3000},
+                new Product(){ Name = "멜론", Price=15000},
+            };
+            list.Sort();
+            foreach(var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
